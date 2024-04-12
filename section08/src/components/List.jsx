479ddab715c35a96700e4c872eaeac33f3fc7d4d
mins1030 <alstj9309@naver.com>
2024-04-12 15:05:15 +0900

    section08 추가

---
+++ section08/src/components/List.jsx
@@ -0,0 +1,37 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const getFilteredDate = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+  const filteredTodos = getFilteredDate();
+
+  return (
+    <div className="List">
+      <h4>Todo List 🍃</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
